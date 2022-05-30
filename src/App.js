14263232dac7e66406df378a392721894cd96a84
Samy Ali <samy.alisanchez91@gmail.com>
2022-05-30 08:30:41 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import BeersDetailsPage from "./pages/BeersDetailsPage";
-/* import NewBeerPage from "./pages/NewBeerPage";
-import RandomBeersPage from "./pages/RandomBeersPage" */
+import NewBeerPage from "./pages/NewBeerPage";
+import RandomBeersPage from "./pages/RandomBeersPage"
 import BeersPage from "./pages/BeersPage"
 import { Routes, Route } from "react-router-dom";
 
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-{/*         <Route exact path="/new-beer" element={<NewBeerPage />} /> */}
-{/*         <Route exact path="/random-beer" element={<RandomBeersPage />} /> */}
+        <Route exact path="/new-beer" element={<NewBeerPage />} />
+        <Route exact path="/random-beer" element={<RandomBeersPage />} />
         <Route exact path="/beers" element={<BeersPage />} />
         <Route exact path="/beers/:id" element={<BeersDetailsPage />} />
         <Route exact path="/" element={<HomePage />} />
